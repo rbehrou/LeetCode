070366ad1686e33bc60186290f03fe62e56d58f6
rbehrou <reza.behrou@colorado.edu>
2022-03-12 18:56:39 -0800

    Added python solution to twoSum problem.

--- twoSum/twoSum.py
+++ twoSum/twoSum.py
@@ -1,27 +1,27 @@
-def twoSum(nums, target):
-    # create a dictionary
-    compDict = dict()
-    res = []
-    
-    # loop over number of elements in list
-    for i in range(len(nums)):
-        # compute the difference between target and current element in list
-        x = target - nums[i]
-        # if current element is in table, then return index of current element from table and i
-        if nums[i] in compDict:
-            res.append(compDict[nums[i]])
-            res.append(i)
-        else:
-            # otherwise store the difference in the dictionary
-            compDict[x] = i
-    # return the result
-    return res
+class Solution:
+    def twoSum(self, nums, target):
+        # create a dictionary
+        compDict = dict()
+        
+        # loop over number of elements in list
+        for i in range(len(nums)):
+            # compute the difference between target and current element in list
+            x = target - nums[i]
+            # if current element is in table, then return index of current element from table and i
+            if nums[i] in compDict:
+                return [compDict[nums[i]], i]
+            else:
+                # otherwise store the difference in the dictionary
+                compDict[x] = i
+
+# call the solution class                
+sol = Solution()
 
 # test case
 nums = [2, 7, 11, 15]
 target = 9
 
 # print response
-res = twoSum(nums,target)
+res = sol.twoSum(nums,target)
 print(res)
 
